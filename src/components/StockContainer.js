2a42dfb83e897bd2a21d3ac6880658d8766c4f00
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-10 19:30:43 -0700

    first deliverable complete

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks}) {
+  console.log(stocks)
+
+  const stockDetail = stocks.map((stock) => {
+    return <Stock key={stock.id} ticker={stock.ticker} name={stock.name} price={stock.price}/>
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockDetail}
     </div>
   );
 }
